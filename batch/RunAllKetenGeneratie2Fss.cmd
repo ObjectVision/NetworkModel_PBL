4aa504b7d9ef1492aea58188ef48ddcd16c0fc44
Jip Claassens <jclaassens@objectvision.nl>
2025-06-04 10:57:11 +0200

    fss ontkoppeling keten generatie

---
+++ batch/RunAllKetenGeneratie2Fss.cmd
@@ -0,0 +1,45 @@
+set geodmsversion=GeoDms17.4.6
+set exe_dir=C:\Program Files\ObjectVision\%geodmsversion%
+set ProgramPath=%exe_dir%\GeoDmsRun.exe
+
+cd ..\cfg
+
+
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/r/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/o/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/l/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/k/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rr/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/ro/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rl/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rk/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/lr/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/ll/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/kr/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rrr/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rro/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rrl/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rrk/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/ror/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/roo/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rol/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rok/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rlr/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rlo/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rll/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rkr/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/rko/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/lrr/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/lro/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/lrl/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/lrk/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/llr/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/lll/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/krr/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/kro/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/krl/CALC
+"%ProgramPath%" main.dms /NetworkSetup/ConfigurationPerRegio/all/PublicTransport/Keten_Generatie/krk/CALC
+
+
+
+pause "Klaar ?"
